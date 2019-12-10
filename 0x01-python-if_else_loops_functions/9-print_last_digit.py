98afe0c00d849e733e6b5ac8d4decceccd0748ae
Gzoref <gzoref@gmail.com>
2019-12-10 14:19:04 -0500

    Last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/bin/nash/python3
+def print_last_digit(number):
+    if number > 0:
+        number %= 10
+    else:
+        number = (number * -1) % 10
+    print(number, end='')
+    return number
