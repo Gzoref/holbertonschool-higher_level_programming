149985972e1a7f20174a0a54a6fe998563140bc7
Gzoref <gzoref@gmail.com>
2019-12-10 14:56:58 -0500

    Fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def fizzbuzz():
+    for num in range(1, 101):
+        if (num % 3 == 0) and (num % 5 == 0):
+            num = 'FizzBuzz'
+        elif num % 3 == 0:
+            num   = 'Fizz'
+        elif num % 5 == 0:
+            num = 'Buzz'
+        print('{}'.format(num), end=' ')
