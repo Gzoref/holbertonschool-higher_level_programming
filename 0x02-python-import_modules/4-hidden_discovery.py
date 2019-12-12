fd9faf364f2b3c187c0a62b9b9101ba99700d25b
Gzoref <gzoref@gmail.com>
2019-12-12 13:13:24 -0500

    Hidden names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
 
-for name in dir(hidden_4):
-    if name[0] == '_':
-        print('{}'.format(name))
+    for name in dir(hidden_4):
+        if name[1] != '_':
+            print(name)
