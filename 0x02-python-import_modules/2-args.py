df616ee55c10154cc0024a06949bfd6bac310879
Gzoref <gzoref@gmail.com>
2019-12-12 12:53:12 -0500

    Args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if length == 1:
     print('{} arguments.'.format(length - 1))
 elif length == 2:
     print('{} argument: '.format(length - 1))
-    print('{}: {}'.format(length - 1, argv[1]))
+    print('{}: {}'.format(1, argv[1]))
 else:
     for i in range(length):
         if i == 0:
