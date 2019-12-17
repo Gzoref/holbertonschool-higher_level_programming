ce597b4aca601c7b700c62536f1469df874295e4
Gzoref <gzoref@gmail.com>
2019-12-17 12:05:08 -0500

    Replace at index

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    if idx > len(my_list):
+    if idx >= len(my_list):
         return my_list
     else:
         my_list[idx] = element
