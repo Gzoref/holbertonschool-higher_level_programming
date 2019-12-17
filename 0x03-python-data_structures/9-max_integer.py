c7e13902f453890283cb8f16d3f28fe34fdf0628
Gzoref <gzoref@gmail.com>
2019-12-17 12:18:37 -0500

    return largest num

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     largest = my_list[0]
-    if not my_list:
+    if len(my_list) < 1:
         return None
     else:
         for num in my_list:
