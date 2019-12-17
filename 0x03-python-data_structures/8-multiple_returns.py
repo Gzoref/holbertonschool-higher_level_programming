261777ae000fa30e50aa5d6764c1c792d95e5f33
Gzoref <gzoref@gmail.com>
2019-12-17 12:14:17 -0500

    Length and first letter

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    first_char = ''
     if not sentence.strip():
-        first_char = None
+        return None
     else:
         return len(sentence), sentence[0]
