2d555ce487295105f6d88c8ee276bc5cfbc02998
Gzoref <gzoref@gmail.com>
2019-12-19 17:15:29 -0500

    Largets value of dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     biggest = []
     for number in a_dictionary:
