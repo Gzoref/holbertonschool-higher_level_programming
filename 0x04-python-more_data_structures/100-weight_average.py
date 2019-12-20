4a825652ba01ebf4653d16abc35ea31fabdf9b7c
Gzoref <gzoref@gmail.com>
2019-12-20 11:12:15 -0500

    Weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pythpn3
+def weight_average(my_list=[]):
+    weight = 0
+    product = []
+    for i in my_list:
+        weight += i[1]
+        product.append(i[0] * i[1])
+    return sum(product) / weight
