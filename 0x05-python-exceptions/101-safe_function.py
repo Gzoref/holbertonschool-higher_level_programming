3a1b3792aa346fd84b8ef4b236e5ff305faaa4c2
Gzoref <gzoref@gmail.com>
2020-01-08 14:26:19 -0500

    Print function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def safe_function(fct, *args):
+    from sys import stderr
+    try:
+        result = fct(*args)
+        return result
+    except Exception as err:
+        print('Exception: {}'.format(err), file=stderr)
+        return None
