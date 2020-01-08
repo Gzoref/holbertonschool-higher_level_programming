48e74c6f33b713686c51d5b878173a8e4479f543
Gzoref <gzoref@gmail.com>
2020-01-08 12:15:08 -0500

    function that prints value

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!usr/bin/python3
-
 def safe_print_integer(value):
     try:
-        print('{}'.format(value))
+        print('{:d}'.format(value))
     except (ValueError, TypeError):
         return False
     return True
