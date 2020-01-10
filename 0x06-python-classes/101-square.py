d90be1fa638a89660e47945107f563a6e75e5861
Gzoref <gzoref@gmail.com>
2020-01-10 13:49:55 -0500

    String rep

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Coordinates of a square"""
+
+
+class Square:
+    """Private instance attribute: size
+    Instantiation with area and position method """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes attribute size """
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """Calculate area of square"""
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """Getter for square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Initializes attribute size """
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Getter for position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Initializes attribute position"""
+        if len(value) is not 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if (type(value[0]) is not int or value[0] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if (type(value[1]) is not int or value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def __str__(self):
+        """Print representation of squares"""
+        str = '\n' * self.__position[1]
+        for i in range(self.size):
+            str += '_' * self.position[0]
+            str += '#' * self.__size + '\n'
+        return str
