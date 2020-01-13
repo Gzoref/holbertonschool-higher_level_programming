5aa3343cb69d56149d0f66f317fba365c2d613a5
Gzoref <gzoref@gmail.com>
2020-01-13 12:25:05 -0500

    Add Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,11 +5,12 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """Function thats prints my name"""
-    try:
-        return first_name + ' ' + last_name
-    except:
-        if isinstance(first_name, str):
-            raise TypeError('first_name must be a string')
-        if isinstance(last_name, str):
-            raise TypeError('last_name must be a string')
+    """
+    Function thats prints my name
+    """
+    if isinstance(first_name, str) is False:
+        raise TypeError('first_name must be a string')
+    if isinstance(last_name, str) is False:
+        raise TypeError('last_name must be a string')
+
+    print('My name is {:s} {:s}'.format(first_name, last_name))
