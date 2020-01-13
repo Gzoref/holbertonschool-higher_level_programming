0337ca33b5be066a872a8f9e1c3ed4aef383ea5d
Gzoref <gzoref@gmail.com>
2020-01-12 19:35:31 -0500

    Matrix divided

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,12 +9,16 @@ def matrix_divided(matrix, div):
     """
     Function that divides a matrix
     """
-    if div is 0:
-        raise ZeroDivisionError('division by zero')
+    if type(matrix) is not list:
+        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
     for item in range(len(matrix)):
         if item is not 0:
             result = item -1
             if len(matrix[item]) is not len(matrix[result]):
                 raise TypeError('Each row of the matrix must have the same size')
+    if isinstance(div, int) is False:
+        raise TypeError('div must be a number')
+    if div is 0:
+        raise ZeroDivisionError('division by zero')
 
     return [[round(item  / div,2) for item in matrix_list] for matrix_list in matrix]
