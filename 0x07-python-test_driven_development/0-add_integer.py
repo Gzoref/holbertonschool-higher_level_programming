ac113b586ce08b0708176b963b31688d01b0578e
Gzoref <gzoref@gmail.com>
2020-01-12 19:38:26 -0500

    Fix pep8

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,12 +3,13 @@
 
 """Integer addition"""
 
+
 def add_integer(a, b=98):
     """
     Function adds two integers
     """
     if isinstance(a, (int, float)) is False:
-        raise TypeError ('a must be an integer')
+        raise TypeError('a must be an integer')
     if isinstance(b, (int, float)) is False:
-            raise TypeError ('b must be an integer')
+            raise TypeError('b must be an integer')
     return int(a) + int(b)
