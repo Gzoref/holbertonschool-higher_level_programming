5aa4f5730e3fdf301cb02fcfc6cf5a37f534a83d
Gzoref <gzoref@gmail.com>
2020-01-13 19:02:27 -0500

    Add 6-max_integer_test.py unit test file

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,31 +1,14 @@
 #!/usr/bin/python3
 """
-Module to find the max integer
+Unittest for max_integer([..])
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
-    """
-    Test the max_integer function
-    """
 
-    def test_max_integer(self):
-        '''
-        Test if integer is max integer
-        '''
-        result = max_integer([1, 4, 3, 2])
-        self.assertEqual(result, 4)
-        result = max_integer([12, 9, 7, 2])
-        self.assertEqual(result, 12)
-
-    def test_isint(self):
-        '''
-        Is it an integer?
-        '''
-        number = 24
-        self.asserTrue(max_integer([number, 2]) == 2)
-
-
-    if __name__ == '__main__':
-        unittest.main()
+    def test_variable(self):
+        a = 77
+        self.assertEqual(max_integer([a]), a)
+            
