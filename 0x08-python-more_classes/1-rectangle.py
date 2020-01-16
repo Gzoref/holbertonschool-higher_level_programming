8cffe7f0ad796fc561a6edbb254357de163aa4f8
Gzoref <gzoref@gmail.com>
2020-01-16 17:19:58 -0500

    Fix raise error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -29,7 +29,7 @@ class Rectangle:
         width setter
         """
         if not isinstance(value, int):
-            TypeError('width must be an integer')
+            raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
@@ -47,7 +47,7 @@ class Rectangle:
         width setter
         """
         if not isinstance(value, int):
-            TypeError('height must be an integer')
+           raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
