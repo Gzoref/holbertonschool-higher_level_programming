3bb2feb7bb2ca394e5a7f30296fc275827b006d6
Gzoref <gzoref@gmail.com>
2020-01-16 14:47:59 -0500

    Add test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """
-Unittest for max_integer([..])
+Unittest for max_integer
 """
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-
-    def variable_test(self):
-        a = 77
-        self.assertEqual(max_integer([a]), a)
+    def empty_list_test(self):
+        self.assertEqual(max_integer([]), None)
 
     def test_max_front(self):
         my_list = [5, 2, 3, 4]
@@ -20,18 +18,29 @@ class TestMaxInteger(unittest.TestCase):
         my_list = [5, 2, 99, 3, 4]
         self.assertEqual(max_integer(my_list), 99)
 
-    def test_end(self):
-        my_list = [1, 2, 3, 4]
-        self.assertEqual(max_integer(my_list), 4)
+    def test_max_end(self):
+        my_list = [1, 2, 3, 4, 56]
+        self.assertEqual(max_integer(my_list), 56)
+
+    def test_max_long(self):
+        my_list = [1, 2, 3, 4, 5, 77, 88, 99, 0, 56]
+        self.assertEqual(max_integer(my_list), 99)
 
     def test_negative(self):
-        my_list = [1, -22, 3, 4]
-        self.assertEqual(max_integer(my_list), 4)
-
-    def empty_list_test(self):
-        my_list = []
-        self.assertEqual(max_integer(my_list), None)
+        my_list = [-1, -22, -3, -4]
+        self.assertEqual(max_integer(my_list), -1)
 
     def mixed_cases_test(self):
         my_list = [2, 3, 4, 5, 'd']
-        self.assertRaises(TypeError, max_integer, my_list)
+        self.assertEqual(max_integer(my_list), "d")
+
+    def isnone_test(self):
+        my_list = []
+        self.assertEqual(max_integer(my_list), None)
+
+    def test_none(self):
+        with self.assertRaises(TypeError):
+            max_integer(None)
+
+    def with_string(self):
+        self.assertEqual(max_integer("string"), "t")
