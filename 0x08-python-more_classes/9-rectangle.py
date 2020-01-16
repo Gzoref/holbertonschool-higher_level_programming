b76b02e3860ca44213e96a9066711162dbf8fe49
Gzoref <gzoref@gmail.com>
2020-01-16 12:42:42 -0500

    Fix TypeError

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -93,14 +93,15 @@ class Rectangle:
         print('Bye rectangle...')
         Rectangle.number_of_instances -= 1
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """
         Returns the biggest rectangle based on the area
         """
         if not isinstance(rect_1, Rectangle):
-            TypeError('rect_1 must be an instance of Rectangle')
+           raise TypeError('rect_1 must be an instance of Rectangle')
         if not isinstance(rect_2, Rectangle):
-            TypeError('rect_2 must be an instance of Rectangle')
+            raise TypeError('rect_2 must be an instance of Rectangle')
         if (rect_1.area() >= rect_2.area()):
             return rect_1
         else:
