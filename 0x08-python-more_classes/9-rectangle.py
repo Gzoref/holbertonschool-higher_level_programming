9aedb535abf6d76b76ac5ff7b7af7d4ae7b2ec0e
Gzoref <gzoref@gmail.com>
2020-01-16 13:09:57 -0500

    Fix raise

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -15,6 +15,14 @@ class Rectangle:
         """
         Initializes private attribute width and height
         """
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
@@ -32,7 +40,7 @@ class Rectangle:
         width setter
         """
         if not isinstance(value, int):
-            TypeError('width must be an integer')
+            raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
@@ -50,7 +58,7 @@ class Rectangle:
         width setter
         """
         if not isinstance(value, int):
-            TypeError('height must be an integer')
+            raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
@@ -59,13 +67,13 @@ class Rectangle:
         """
         Returns a rectangle's area
         """
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
         """
         Returns a rectangle's perimeter
         """
-        if self.width == 0 or self.height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return (self.width + self.height) * 2
 
@@ -74,8 +82,6 @@ class Rectangle:
         String representation method
         """
         string = ""
-        if self.width == 0 or self.height == 0:
-            return string
         for i in range(self.height):
             string += str(self.print_symbol) * self.width + '\n'
         return string[:-1]
@@ -84,7 +90,7 @@ class Rectangle:
         """
         Representation
         """
-        return "Rectangle({}, {})".format(self.width, self.height)
+        return "Rectangle({}, {})".format(self.__width, self.__height)
 
     def __del__(self):
         """
@@ -102,14 +108,13 @@ class Rectangle:
             raise TypeError('rect_1 must be an instance of Rectangle')
         if not isinstance(rect_2, Rectangle):
             raise TypeError('rect_2 must be an instance of Rectangle')
-        if (rect_1.area() >= rect_2.area()):
+        if rect_1.area() == rect_2.area():
+            return rect_1
+        elif rect_1.area() > rect_2.area():
             return rect_1
         else:
             return rect_2
 
     @classmethod
     def square(cls, size=0):
-        """
-        Returns a new Rectangle instance with width == height == size
-        """
         return cls(size, size)
