69463f64bfa90d1b97f4e80ea8acfb531ba84f7e
Gzoref <gzoref@gmail.com>
2020-01-20 11:24:56 -0500

    A class MyList that inherits from list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+A class MyList that inherits from list
+'''
+
+class MyList(list):
+    '''
+    MyList class
+    '''
+        
+    def print_sorted(self):
+        print(sorted(self))
