54103d97df86629e26a39bfd2007d5a8c03da102
Gzoref <gzoref@gmail.com>
2020-01-21 13:08:44 -0500

    Write lines

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -6,6 +6,9 @@ Write a function that writes a string to a text file
 
 
 def write_file(filename="", text=""):
+    '''
+    Write to a file
+    '''
     with open(filename, 'w', encoding='utf-8') as f:
         file_write = f.write(text)
         f.close()
