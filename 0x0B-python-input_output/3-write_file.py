ca8c09ebde31ce2768aaae879570f29cd89da9d1
Gzoref <gzoref@gmail.com>
2020-01-21 11:15:31 -0500

    Write to a file

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+Write a function that writes a string to a text file
+(UTF8) and returns the number of characters written:
+'''
+
+
+def write_file(filename="", text=""):
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
