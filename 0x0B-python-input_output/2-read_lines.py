88d0a4a14c874a0f518eabd34a64fa7859915313
Gzoref <gzoref@gmail.com>
2020-01-21 12:57:52 -0500

    Read n lines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -7,7 +7,7 @@ Write a function that reads n lines of a text file
 
 def read_lines(filename="", nb_lines=0):
     line_count = 0
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
             line_count += 1
             print(line, end='')
