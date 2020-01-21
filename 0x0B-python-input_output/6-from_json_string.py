0bb9d8579230d9c683aa09917b3a70114120506e
Gzoref <gzoref@gmail.com>
2020-01-21 16:57:27 -0500

    Pep8 error

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -12,8 +12,4 @@ def from_json_string(my_str):
     '''
     Return object as JSON
     '''
-    try:
-        json_var = json.loads(my_str)
-    except ValueError:
-        raise ValueError('Expecting property name enclosed in double quotes:')
-    return json_var
+    return json.loads(my_str)
