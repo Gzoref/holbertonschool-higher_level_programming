b179469062cc57f234506e9d970723cc90ab8a9b
Gzoref <gzoref@gmail.com>
2020-01-21 10:08:32 -0500

    Fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,8 +2,7 @@
 '''
 Write a function that returns True
 if the object is an instance of a class
-that inherited (directly or indirectly)
-from the specified class ; otherwise False.
+that inherited
 '''
 
 
