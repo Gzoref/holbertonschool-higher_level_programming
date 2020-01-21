faac10813bba39a806088c9b89d4eb37fcec9317
Gzoref <gzoref@gmail.com>
2020-01-21 12:35:16 -0500

    pep8 fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,7 +8,9 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-
+    '''
+    Rectangle   
+    '''
     def __init__(self, width, height):
         super().integer_validator('width', width)
         self.__width = width
