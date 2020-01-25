5a6a2bb3a7e1f83af73c940b352c3e99e44a92cd
Gzoref <gzoref@gmail.com>
2020-01-25 01:44:32 -0500

    Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,11 +4,60 @@ Write the class Square that
 inherits from Rectangle:
 '''
 from models.rectangle import Rectangle
+
+
 class Square(Rectangle):
     '''
     Square class inherits from Rectangle
     '''
 
     def __init__(self, size, x=0, y=0, id=None):
+        '''
+        Constructor
+        '''
         super().__init__(size, size, x, y, id)
-        self.size = size
+
+    @property
+    def size(self):
+        '''
+        size getter
+        '''
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        '''
+        size setter
+        '''
+        self.width = value
+        self.height = value
+        
+    def update(self, *args, **kwargs):
+        '''
+        Makes args variadic
+        '''
+        argc = len(args)
+        if argc > 0:
+            try:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[3]
+                self.y = args[4]
+            except IndexError:
+                pass
+        else:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
+                
+    def __str__(self):
+        '''
+        String representation
+        '''
+        return '[Rectangle] ({}) {}/{} - {} '.format(self.id,
+                                                        self.x, self.y, self.width)
