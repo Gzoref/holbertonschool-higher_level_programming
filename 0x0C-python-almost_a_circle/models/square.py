e50b52c1f47d06c335dbc429bd076109e35be409
Gzoref <gzoref@gmail.com>
2020-01-25 00:58:35 -0500

    Fixed __init__

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,13 +3,12 @@
 Write the class Square that
 inherits from Rectangle:
 '''
-from models.base import Base
-
+from models.rectangle import Rectangle
 class Square(Rectangle):
     '''
     Square class inherits from Rectangle
     '''
 
     def __init__(self, size, x=0, y=0, id=None):
-        super()__init__(size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
         self.size = size
