3669965b9b05150d6d4c2a7f13cad9e2822fd538
Gzoref <gzoref@gmail.com>
2020-01-25 15:33:08 -0500

    Added save_to_file method to Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 Create a folder named models with an empty file __init__.py
 inside - with this file, the folder will become a Python module
 '''
+import json
+
 
 class Base:
     '''
@@ -20,3 +22,29 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        '''
+        Turn dictionary representation
+        into JSON
+        '''
+        if list_dictionaries is None:
+            return []
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''
+        Saves JSON of dictionary
+        to a file <Class name>.json
+        '''
+        listToDictionary = []
+        if not list_objs:
+            list_objs = []
+        for items in list_objs:
+            listToDictionary.append(items.to_dictionary())
+        
+        with open('{}'.format(cls.__name__), 'w', encoding='utf-8') as file:
+            file.write(cls.to_json_string(listToDictionary))
+        
