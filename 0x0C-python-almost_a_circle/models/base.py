b1065ba763a0364a31141667f976b47187690375
Gzoref <gzoref@gmail.com>
2020-01-27 16:59:36 -0500

    pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,8 +32,7 @@ class Base:
         '''
         if list_dictionaries is None:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @staticmethod
     def from_json_string(json_string):
