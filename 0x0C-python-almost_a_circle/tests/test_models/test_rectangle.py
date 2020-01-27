76f5013d6fca877a41169dcaf4bf8363f3dad42f
Gzoref <gzoref@gmail.com>
2020-01-27 15:29:02 -0500

    Rectangle tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,13 +1,15 @@
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
+import pep8
+import json
 
 class TestRectangleClass(unittest.TestCase):
     
     @classmethod
     def setUp(self):
-       Base.base_instance = 0
-       pass
+        pass
             
     @classmethod
     def tearDownClass(self):
@@ -19,9 +21,27 @@ class TestRectangleClass(unittest.TestCase):
         '''
         self.assertTrue(str(Rectangle), "<class 'models.rectangle.Rectangle'>")
         
+    def test_pep8(self):
+        '''
+        Test pep8
+        '''
+        pep8_style = pep8.StyleGuide(quit=True)
+        pep_check = pep8_style.check_files(['models/rectangle.py'])
+        self.assertEqual(pep_check.total_errors, 0, 'Pep8 Error in file')
+        
     def test_B_inheritance(self):
-        '''Tests if Rectangle inherits Base.'''
+        '''
+        Tests if Rectangle inherits Base.
+        '''
         self.assertTrue(issubclass(Rectangle, Base))
+    
+    def test_id(self):
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 1)
+        r2 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r2.id, 12)
+    
+
     
     if __name__ == '__main__':
         unittest.main()
