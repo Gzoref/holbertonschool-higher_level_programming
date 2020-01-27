161d47addf14d79cb84ea515c360be94858b844f
Gzoref <gzoref@gmail.com>
2020-01-27 16:51:31 -0500

    pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -30,7 +30,7 @@ class Base:
         into JSON
         '''
         if list_dictionaries is None and len(list_dictionaries) is 0:
-            return '[]'
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
 
