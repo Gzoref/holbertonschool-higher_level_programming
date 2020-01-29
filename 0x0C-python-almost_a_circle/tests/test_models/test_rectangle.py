a2720cfc8f17df6fe03c4ef04306cbabe4d3bb5c
Gzoref <gzoref@gmail.com>
2020-01-28 19:57:36 -0500

    More tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -9,8 +9,13 @@ import json
 class TestRectangleClass(unittest.TestCase):
 
     @classmethod
-    def setUp(self):
+    def setUp(cls):
         Base._Base__nb_objects = 0
+        cls.rect1 = Rectangle(2, 4)
+        cls.rect2 = Rectangle(2, 4, 6)
+        cls.rect2 = Rectangle(2, 4, 6, 8)
+        cls.rect2 = Rectangle(2, 4, 6, 8 ,10)
+
 
     @classmethod
     def tearDownClass(self):
@@ -27,9 +32,30 @@ class TestRectangleClass(unittest.TestCase):
         Tests if Rectangle inherits Base.
         '''
         self.assertTrue(issubclass(Rectangle, Base))
+     
+    def test_id_4(self):
+        b1 = Base(1)
+        b2 = Base(2)
+        b3 = Base(3)
+        b4 = Base(69)
+        b5 = Base(4)
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+        self.assertEqual(b4.id, 69)
+        self.assertEqual(b5.id, 4)
 
     def test_id(self):
         r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 1)
+        self.assertEqual(r1.id, 4)
         r2 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r2.id, 12)
+    
+    def test_to_dict(self):
+        dict1 = self.rect1.to_dictionary()
+        self.assertEqual({'x': 0, 'y': 0, 'id': 1, 'height': 4, 'width': 2}, dict1)
+        
+    def test_area(self):
+        r1 = Rectangle(10, 10, 10, 10, 42)
+        self.assertEqual(str(r1), '[Rectangle] (42) 10/10 - 10/10')
+        
