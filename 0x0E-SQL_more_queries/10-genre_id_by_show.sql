6812d39eeb8e9e3f115079ae8e76ad7827adcbc5
Gzoref <gzoref@gmail.com>
2020-03-03 16:08:30 -0500

    10. Genre ID by show

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+WHERE tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
