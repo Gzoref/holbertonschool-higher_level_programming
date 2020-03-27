e489210bda5e3a76d563277d7d620cb10540558c
Gzoref <gzoref@gmail.com>
2020-03-27 18:26:22 -0400

    Make safe from SQL injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+'''
+Take in argyment but make it s safe from MySQL injections.
+'''
+import MySQLdb
+from sys import argv
+
+
+conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                       user=argv[1], passwd=argv[2], db=argv[3])
+cur = conn.cursor()
+cur.execute("SELECT * FROM states WHERE name=%s ORDER BY states.id",
+            (argv[4], ))
+query_rows = cur.fetchall()
+for row in query_rows:
+        print(row)
+cur.close()
+conn.close()
