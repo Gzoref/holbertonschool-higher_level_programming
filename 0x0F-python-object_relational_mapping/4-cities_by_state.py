e7e171ff0f8eb526d3d45e088f7d6d8b07052951
Gzoref <gzoref@gmail.com>
2020-03-27 19:19:32 -0400

    List cities by id

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+Lists all cities from the database hbtn_0e_4_usa.
+'''
+
+import MySQLdb
+from sys import argv
+
+
+conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                       user=argv[1], passwd=argv[2], db=argv[3])
+
+cur = conn.cursor()
+cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN "
+            "states ON cities.state_id = states.id")
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
