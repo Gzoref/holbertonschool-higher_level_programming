f87d789079f9af73c290a100b58dcb1125472e7f
Gzoref <gzoref@gmail.com>
2020-03-28 16:56:52 -0400

    Add if guard

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,14 +8,15 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-        conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                               user=argv[1], passwd=argv[2], db=argv[3])
+
+        conn = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
         cur = conn.cursor()
+
         cur.execute("SELECT * FROM states\
-                     WHERE name='{}' ORDER BY states.id ASC"
-                    .format(argv[4]))
+                     WHERE name='{}' ORDER BY states.id ASC".format(argv[4]))
         query_rows = cur.fetchall()
+
         for row in query_rows:
-                print(row)
+                if row[1] == argv[4]:
+                        print(row)
         cur.close()
-        conn.close()
