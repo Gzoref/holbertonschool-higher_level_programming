c3d5732e298b16c049e893bf81e9ba3fc2cb96c1
Gzoref <gzoref@gmail.com>
2020-03-28 13:59:24 -0400

    Add main syntax

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,15 +6,16 @@ Lists all cities from the database hbtn_0e_4_usa.
 import MySQLdb
 from sys import argv
 
+if __name__ == "__main__":
 
-conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                       user=argv[1], passwd=argv[2], db=argv[3])
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
 
-cur = conn.cursor()
-cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN "
-            "states ON cities.state_id = states.id")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-conn.close()
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN "
+                "states ON cities.state_id = states.id")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
