96a56a8cc6e09f648627a5aea789010df7bf400f
Gzoref <gzoref@gmail.com>
2020-03-27 20:48:43 -0400

    Links state and inherits from base

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,6 +13,6 @@ class State(Base):
     State class inherits from Base
     '''
     __tablename__ = 'states'
-    
+
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
