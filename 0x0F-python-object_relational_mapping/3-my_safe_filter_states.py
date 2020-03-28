6aebc02ca6acbff62902f0174be2f791a457e9e1
Gzoref <gzoref@gmail.com>
2020-03-28 13:57:47 -0400

    Add main syntax

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,10 +8,10 @@ from sys import argv
 if __name__ == "__main__":
 
         conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                       user=argv[1], passwd=argv[2], db=argv[3])
+                               user=argv[1], passwd=argv[2], db=argv[3])
         cur = conn.cursor()
         cur.execute("SELECT * FROM states WHERE name=%s ORDER BY states.id",
-                (argv[4], ))
+                    (argv[4], ))
         query_rows = cur.fetchall()
         for row in query_rows:
                 print(row)
