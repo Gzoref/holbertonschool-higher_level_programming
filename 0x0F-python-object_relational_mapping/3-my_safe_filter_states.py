592f793006dcdabc115c65ebf67e1bc4486003c9
Gzoref <gzoref@gmail.com>
2020-03-28 13:56:39 -0400

    Add main syntax

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,14 +5,15 @@ Take in argyment but make it s safe from MySQL injections.
 import MySQLdb
 from sys import argv
 
+if __name__ == "__main__":
 
-conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+        conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                        user=argv[1], passwd=argv[2], db=argv[3])
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name=%s ORDER BY states.id",
-            (argv[4], ))
-query_rows = cur.fetchall()
-for row in query_rows:
-        print(row)
-cur.close()
-conn.close()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states WHERE name=%s ORDER BY states.id",
+                (argv[4], ))
+        query_rows = cur.fetchall()
+        for row in query_rows:
+                print(row)
+        cur.close()
+        conn.close()
