bed4e429979f100ca5cd3ca8fbe6c6a0ae47902e
Gzoref <gzoref@gmail.com>
2020-03-28 13:36:30 -0400

    Add main syntax

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -10,9 +10,8 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
-
 engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                       format(argv[1], argv[2], argv[3],pool_pre_ping=True))
+                       format(argv[1], argv[2], argv[3], pool_pre_ping=True))
 
 state_name = argv[4]
 
