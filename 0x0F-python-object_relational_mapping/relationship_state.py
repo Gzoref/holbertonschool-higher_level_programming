938b934d658e5e5110d6d5d7c6811a66549fc9ef
Gzoref <gzoref@gmail.com>
2020-03-28 17:13:23 -0400

    relationals

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+Class definition of a State and an instance Base = declarative_base():
+'''
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    '''
+    State class inherits from Base
+    '''
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref=backref('states',
+                                                  cascade='all', delete))
