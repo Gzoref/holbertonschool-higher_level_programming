9fabde051528364f159896e32689f383468f16b6
Gzoref <gzoref@gmail.com>
2020-03-28 14:08:10 -0400

    Add main syntax

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -9,24 +9,24 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+if __name__ == "__main__":
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                       format(argv[1], argv[2], argv[3], pool_pre_ping=True))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(argv[1], argv[2], argv[3],
+                                  pool_pre_ping=True))
 
-state_name = argv[4]
+    state_name = argv[4]
 
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    for state in session.query(State).order_by(State.id):
+        if state.name == state_name:
+            print('{}'.format(state.id))
+            '''
+            Not sure why only exit() gets correct output!!!
+            '''
+            exit()
+            print('Not found')
 
-
-for state in session.query(State).order_by(State.id):
-    if state.name == state_name:
-        print('{}'.format(state.id))
-        '''
-        Not sure why only exit() gets correct output!!!
-        '''
-        exit()
-print('Not found')
-
-session.close()
+    session.close()
