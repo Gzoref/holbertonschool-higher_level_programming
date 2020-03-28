9914b52c2e5f8ed1fcc735fba5843b348832b4b6
Gzoref <gzoref@gmail.com>
2020-03-28 13:47:42 -0400

    Add main syntax

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,14 +7,14 @@ states table of hbtn_0e_0_usa where name matches the argument.
 import MySQLdb
 from sys import argv
 
-
-conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                       user=argv[1], passwd=argv[2], db=argv[3])
-cur = conn.cursor()
-cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY states.id".
-            format(argv[4]))
-query_rows = cur.fetchall()
-for row in query_rows:
-        print(row)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+        conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                               user=argv[1], passwd=argv[2], db=argv[3])
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY states.id".
+                    format(argv[4]))
+        query_rows = cur.fetchall()
+        for row in query_rows:
+                print(row)
+        cur.close()
+        conn.close()
