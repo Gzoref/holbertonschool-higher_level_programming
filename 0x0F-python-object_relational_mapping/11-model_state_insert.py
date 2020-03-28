28d4aaea835419a82b4dccd300fb88bd8c6c3ea6
Gzoref <gzoref@gmail.com>
2020-03-27 23:18:25 -0400

    Adds Louisiana to db

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Adds the State object “Louisiana” to the database hbtn_0e_6_usa
+'''
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                       format(argv[1], argv[2], argv[3],pool_pre_ping=True))
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+new_state = State(name='Louisiana')
+session.add(new_state)
+session.commit()
+print('{}'.format(new_state.id))
+session.close()
