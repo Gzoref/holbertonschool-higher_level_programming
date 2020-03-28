e79c7d21b092e6c7029659e155ff2a3258152809
Gzoref <gzoref@gmail.com>
2020-03-28 14:00:38 -0400

    Add main syntax

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,16 +6,17 @@ Lists all cities from the database hbtn_0e_4_usa.
 import MySQLdb
 from sys import argv
 
+if __name__ == "__main__":
 
-conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                       user=argv[1], passwd=argv[2], db=argv[3])
+    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                           user=argv[1], passwd=argv[2], db=argv[3])
 
-cur = conn.cursor()
-cur.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN\
+    cur = conn.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN\
             states ON cities.state_id = states.id WHERE states.name=%s\
             ORDER by cities.id ASC;",
-            (argv[4], ))
-query_rows = cur.fetchall()
-print(', '.join([row[1] for row in query_rows]))
-cur.close()
-conn.close()
+                (argv[4], ))
+    query_rows = cur.fetchall()
+    print(', '.join([row[1] for row in query_rows]))
+    cur.close()
+    conn.close()
