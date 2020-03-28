d9de024a38a7b17d7bba33b4a28ab3a4c05c82df
Gzoref <gzoref@gmail.com>
2020-03-28 13:46:14 -0400

    Add main syntax

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ from sys import argv
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                       user=argv[1], passwd=argv[2], db=argv[3])
+                           user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
