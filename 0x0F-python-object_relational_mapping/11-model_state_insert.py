12cb85fd1c0fedd062f55def9737ee46157a2607
Gzoref <gzoref@gmail.com>
2020-03-28 14:09:55 -0400

    Add main syntax

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -9,16 +9,17 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
+if __name__ == "__main__":
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                       format(argv[1], argv[2], argv[3],pool_pre_ping=True))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(argv[1], argv[2], argv[3],
+                                  pool_pre_ping=True))
 
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-Session = sessionmaker(bind=engine)
-session = Session()
-
-new_state = State(name='Louisiana')
-session.add(new_state)
-session.commit()
-print('{}'.format(new_state.id))
-session.close()
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+    print('{}'.format(new_state.id))
+    session.close()
