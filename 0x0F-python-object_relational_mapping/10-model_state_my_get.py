e67783d3bf92c48979d67dd1d0e950595d439c43
Gzoref <gzoref@gmail.com>
2020-03-27 23:08:15 -0400

    Print id of state givern as arg

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+Prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa
+'''
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                       format(argv[1], argv[2], argv[3],pool_pre_ping=True))
+
+state_name = argv[4]
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+for state in session.query(State).order_by(State.id):
+    if state.name == state_name:
+        print('{}'.format(state.id))
+        '''
+        Not sure why only exit() gets correct output!!!
+        '''
+        exit()
+print('Not found')
+
+session.close()
