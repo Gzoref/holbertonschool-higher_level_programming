b3a8b9a0d6b113e7014d06caeeed39825965dce7
Gzoref <gzoref@gmail.com>
2020-03-27 21:41:54 -0400

    List cities by their state

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+lists all State objects from the database hbtn_0e_6_usa
+'''
+import MySQLdb
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                       format(argv[1], argv[2], argv[3],pool_pre_ping=True))
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+for state in session.query(State).order_by(State.id):
+    print('{}: {}'.format(state.id, state.name))
+
+
+session.close()
