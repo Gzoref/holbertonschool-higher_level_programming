1b950bedf3d581622cefedc7ddd5d20403ed31ca
Gzoref <gzoref@gmail.com>
2020-03-28 13:43:11 -0400

    Add main syntax

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
         cur.execute("SELECT * FROM states ORDER BY states.id ASC")
         query_rows = cur.fetchall()
         for row in query_rows:
-                print(row)
+                if row[1].startswith('N'):
+                        print(row)
         cur.close()
         conn.close()
