e3adefc72771e8698041a464da7699cb7cdfb884
Gzoref <gzoref@gmail.com>
2020-03-28 13:37:43 -0400

    Add main syntax

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,14 +6,14 @@ Lists all states with a name starting with N
 import MySQLdb
 from sys import argv
 
+if __name__ == "__main__":
 
-conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                       user=argv[1], passwd=argv[2], db=argv[3])
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    if row[1].startswith('N'):
-        print(row)
-cur.close()
-conn.close()
+        conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                               user=argv[1], passwd=argv[2], db=argv[3])
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+        query_rows = cur.fetchall()
+        for row in query_rows:
+                print(row)
+        cur.close()
+        conn.close()
