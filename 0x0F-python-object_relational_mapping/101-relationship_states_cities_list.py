236d05b242def7b510465cae1cb47f8d1f5198b9
Gzoref <gzoref@gmail.com>
2020-03-29 14:17:56 -0400

    lists all State objects, and corresponding City objects,

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     session = Session()
 
     for state in session.query(State).order_by(State.id):
-        print('{}: {} '.format(state.id, state.name))
+        print('{}: {}'.format(state.id, state.name))
         for city in state.cities:
             print('\t{}: {}'.format(city.id, city.name))
 
