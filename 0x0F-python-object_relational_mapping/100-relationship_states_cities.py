9b179fa89916ad6db794be3e749e46ba8f724e9c
Gzoref <gzoref@gmail.com>
2020-03-29 14:03:18 -0400

    creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -21,10 +21,11 @@ if __name__ == "__main__":
     session = Session()
 
     new_state = State(name='California')
-    new_city = City(name='San Francisco', state=new_state)
-    new_state.cities.append(new_city)
-
     session.add(new_state)
+    session.commit()
+
+    new_city = City(name='San Francisco', state=new_state)
     session.add(new_city)
+    session.commit()
 
     session.close()
