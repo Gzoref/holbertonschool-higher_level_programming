e469812d07e8915949c22d0e6716fabe9e532ef6
Gzoref <gzoref@gmail.com>
2020-04-07 19:59:36 -0400

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+Finds a peak in a list of unsorted integers.
+'''
+
+
+def find_peak(list_of_integers):
+    '''
+    Finds the peak integer
+    '''
+    list_length = len(list_of_integers)
+    end = list_length - 1
+    if not list_of_integers:
+        return None
+    if list_of_integers[0] >= list_of_integers[1]:
+        return list_of_integers[0]
+    if list_of_integers[end] > list_of_integers[end - 1]:
+        return list_of_integers[end]
+    for nums in range(1, end):
+        if list_of_integers[nums] > list_of_integers[1 + 1] and\
+           list_of_integers[nums] > list_of_integers[nums - 1]:
+            return list_of_integers[nums]
+    return None
