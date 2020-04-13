7b28ca1b3ba3ffac394243fdd043b1d4f6620102
Gzoref <gzoref@gmail.com>
2020-04-13 14:49:14 -0400

    What's my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ url = 'https://intranet.hbtn.io/status'
 with request.urlopen(url) as response:
     the_page = response.read()
     print('Body response:')
-    print('- type: {}'.format(type(the_page)))
-    print('- content: {}'.format(the_page))
-    print('- utf8 content: {}'.format(the_page.decode('utf-8')))
+    print('\t- type: {}'.format(type(the_page)))
+    print('\t- content: {}'.format(the_page))
+    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
