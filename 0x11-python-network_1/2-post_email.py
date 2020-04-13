b3e04c3fddd4e5254c61aebafb2b717fc4d5125b
Gzoref <gzoref@gmail.com>
2020-04-13 15:21:01 -0400

    POST an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+Script that sends a request to the URL and displays the body of the response
+'''
+
+from urllib import request, parse
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    values = {'email' : sys.argv[2]}
+
+    values = parse.urlencode(values).encode('utf-8')
+    req = request.Request(url, values)
+
+    with request.urlopen(req) as response:
+        the_page = response.read()
+        the_page = response.decode('utf-8')
+        print(the_page)
