05f253daf9e54c217d70c5babdbd50e1c39188ec
Gzoref <gzoref@gmail.com>
2020-04-13 15:16:56 -0400

    Response hader value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,5 +5,6 @@ Script that fetches https://intranet.hbtn.io/status
 from urllib import request
 import sys
 
-with request.urlopen(sys.argv[1]) as response:
-    print(response.getheader('X-Request-Id'))
+if __name__ == '__main__':
+    with request.urlopen(sys.argv[1]) as response:
+        print(response.getheader('X-Request-Id'))
