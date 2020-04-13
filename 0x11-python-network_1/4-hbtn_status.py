060f454c72d1bb8fac2c5a6de784bf2bf9fd3a74
Gzoref <gzoref@gmail.com>
2020-04-13 17:58:21 -0400

    Whats my status #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,8 +4,8 @@ Script that fetches https://intranet.hbtn.io/status
 '''
 import requests
 
-url = requests.get('https://intranet.hbtn.io/status')
-print('Body response:')
-print('\t- type: {}'.format(type(url.text)))
-if url.status_code == 200:
-    print('\t- content: OK')
+if __name__ == "__main__":
+    url = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(url.text)))
+    print('\t- content: {}'.format(url.text))
