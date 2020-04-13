2efe1d72c163e29b53b05ee45951550368715577
Gzoref <gzoref@gmail.com>
2020-04-13 16:11:47 -0400

    POST an email #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 '''
-That takes in a URL and an email address, sends a POST request to the 
-passed URL with the email as a parameter, and finally displays the body of the response.
+That takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and finally
+displays the body of the response.
 '''
 
 import requests
 import sys
 
 if __name__ == "__main__":
-    email = {'email' : sys.argv[2]}
+    email = {'email': sys.argv[2]}
     response = requests.post(sys.argv[1], email)
     print('{}'.format(response.text))
-    
