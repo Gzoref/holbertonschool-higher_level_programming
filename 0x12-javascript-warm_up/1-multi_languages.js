bc43d08c10e0b987eba83a08c21c25e9b5a07bea
Gzoref <gzoref@gmail.com>
2020-05-05 13:30:46 -0400

    Log multiple language phrases

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const phrase1 = 'C is fun';
+const phrase2 = 'Python is cool';
+const phrase3 = 'Javascript is amazing';
+
+console.log(phrase1);
+console.log(phrase2);
+console.log(phrase3);
