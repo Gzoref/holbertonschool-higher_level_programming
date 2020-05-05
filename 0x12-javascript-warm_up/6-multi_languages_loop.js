0151e0ec81c21102433384f4b0e53048404f9a2b
Gzoref <gzoref@gmail.com>
2020-05-05 14:04:42 -0400

    loop language statements

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let index = 0; index < myArray.length; index++) {
+  const element = myArray[index];
+  console.log(element);
+}
