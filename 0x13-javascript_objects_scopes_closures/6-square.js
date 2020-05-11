b3b060e397596e95328002bca2a2f4266f8146e1
Gzoref <gzoref@gmail.com>
2020-05-11 14:05:22 -0400

    Square inherits Rect print C or X

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let index = 0; index < this.height; index++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
