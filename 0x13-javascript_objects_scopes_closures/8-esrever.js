612c1ef1824716ab1a8775ed3ff3fcded976bfaa
Gzoref <gzoref@gmail.com>
2020-05-11 15:18:38 -0400

    Reverse of a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  const revereList = [];
+  let listLength = list.length - 1;
+
+  while (listLength >= 0) {
+    revereList.push(list[listLength]);
+    listLength--;
+  }
+  return revereList;
 };
