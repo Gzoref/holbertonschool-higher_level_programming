2423e65ad6b8e33207b88e7a9ccaeb683e01a319
Gzoref <gzoref@gmail.com>
2020-05-11 16:14:10 -0400

    Number conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-    return function (number) {
-        return number.toString(base);
-    }
+  return function (number) {
+    return number.toString(base);
+  };
 };
