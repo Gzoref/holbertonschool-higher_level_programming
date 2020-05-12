34c3434c7ddf6ec55cacbd320e07cca3b9fecc56
Gzoref <gzoref@gmail.com>
2020-05-12 19:40:29 -0400

    SW characters

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const request = require('request');
+const filmId = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+  for (const characterId of JSON.parse(body).characters) {
+    console.log(result.name);
+  }
+});
