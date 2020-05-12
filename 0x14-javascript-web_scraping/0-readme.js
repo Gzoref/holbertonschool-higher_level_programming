e045583af1d8350e0dfc198485b7777da93daf2e
Gzoref <gzoref@gmail.com>
2020-05-12 13:01:53 -0400

    Read text in file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
